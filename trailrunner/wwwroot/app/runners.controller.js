267d1c8cbd3799fe959fdc82da612ae839316963
Shayne Boyer <shayne.p.boyer@disney.com>
2015-11-22 10:14:54 -0500

    removed Service used just http

--- trailrunner/wwwroot/app/runners.controller.js
+++ trailrunner/wwwroot/app/runners.controller.js
@@ -1,12 +1,12 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
     angular
         .module('app')
         .controller('RunnersController', RunnersController);
 
-    RunnersController.$inject = ['RunnersService'];
-    function RunnersController(RunnersService) {
+    RunnersController.$inject = ['$http'];
+    function RunnersController($http) {
         var vm = this;
         vm.runners = [];
 
@@ -14,18 +14,14 @@
 
         function activate() {
             console.log('activated');
-           // vm.runners = [{"first": "Shayne", "last": "Boyer", "favoriteBeer": "Guiness"}];
-           // console.log(vm.runners);
 
-            vm.runners = function(){
-               RunnersService.getRunners()
-                .success(function(data){
+            $http.get('/api/runner')
+                .success(function (data) {
                     vm.runners = data;
                 })
-                .error(function(http, status, func, httpObj){
+                .error(function (http, status, func, httpObj) {
                     console.log('error getting data', http, status, func, httpObj);
                 });
-            };
         }
     }
 })();
