b17473c314828a417e68f289a0217b698e49980c
Shayne Boyer <shayne.p.boyer@disney.com>
2015-11-21 21:03:46 -0500

    added CameCasePropertyNamesResolver

--- trailrunner/Startup.cs
+++ trailrunner/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace trailrunner
 {
@@ -23,7 +24,10 @@ namespace trailrunner
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options => {
+               options.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
